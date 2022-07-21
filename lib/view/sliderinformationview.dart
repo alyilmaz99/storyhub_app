c258ccce781df35c8fede888db2f6aaf6dafa0a8
Abdullah DÜNDAR <64316056+abdullahD00@users.noreply.github.com>
2022-07-21 18:58:09 +0300

    intro information screen viewmodel  create

--- lib/view/sliderinformationview.dart
+++ lib/view/sliderinformationview.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intro_slider/intro_slider.dart';
-import 'package:storyhub/view/home_page.dart';
+import 'package:storyhub/viewmodel/sliderinformationviewmodel.dart';
 
 class SliderInformationView extends StatefulWidget {
   const SliderInformationView({Key? key}) : super(key: key);
@@ -9,44 +9,7 @@ class SliderInformationView extends StatefulWidget {
   State<SliderInformationView> createState() => _SliderInformationViewState();
 }
 
-class _SliderInformationViewState extends State<SliderInformationView> {
-  List<Slide> slides = [];
-
-  @override
-  void initState() {
-    super.initState();
-    slides.add(
-      Slide(
-        title: "STORY HUB1",
-        description: "Allow miles wound place the leave had. To sitting subject no improve studied limited",
-        pathImage: "assets/images/1.png",
-        backgroundColor: const Color(0xfff5a623),
-      ),
-    );
-    slides.add(
-      Slide(
-        title: "STORY HUB2",
-        description: "Ye indulgence unreserved connection alteration appearance",
-        pathImage: "assets/images/2.jpg",
-        backgroundColor: const Color(0xff203152),
-      ),
-    );
-    slides.add(
-      Slide(
-        title: "STORY HUB3",
-        description:
-            "Much evil soon high in hope do view. Out may few northward believing attempted. Yet timed being songs marry one defer men our. Although finished blessing do of",
-        pathImage: "assets/images/3.png",
-        backgroundColor: const Color(0xff9932CC),
-      ),
-    );
-  }
-
-  void onDonePress() {
-    // Do what you want
-    Navigator.push(context, MaterialPageRoute(builder: (context) => const HomePage()));
-  }
-
+class _SliderInformationViewState extends SliderInformationViewModel {
   @override
   Widget build(BuildContext context) {
     return IntroSlider(
