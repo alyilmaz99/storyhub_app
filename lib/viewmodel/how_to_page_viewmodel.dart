55d88ec5cb9eedd7cf4afeb5fb1813cd22a08e35
alyilmaz99 <aliyilmaz9977@gmail.com>
2022-08-13 19:04:54 +0300

    info modal and const add

--- lib/viewmodel/how_to_page_viewmodel.dart
+++ lib/viewmodel/how_to_page_viewmodel.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:storyhub/const/how_to_const.dart';
 import 'package:storyhub/view/how_to_detail_view.dart';
 import 'package:storyhub/view/how_to_page_view.dart';
 
@@ -11,21 +10,4 @@ abstract class HowToPageViewModal extends State<HowToPageView> {
       MaterialPageRoute(builder: (context) => const HowToDetailView()),
     );
   }
-
-  static const List<HowToModal> details = <HowToModal>[
-    HowToModal(
-        title: "Oyun Kuralları", icon: Icons.rule, body: HowToConst.gameRule1),
-  ];
-}
-
-class HowToModal {
-  const HowToModal({
-    required this.title,
-    required this.icon,
-    required this.body,
-  });
-
-  final String title;
-  final IconData icon;
-  final String body;
 }
