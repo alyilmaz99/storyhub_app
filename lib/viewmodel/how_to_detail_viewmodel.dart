77ef6cba80378befebbad08f81d0b85ef4886ace
alyilmaz99 <aliyilmaz9977@gmail.com>
2022-08-13 13:55:03 +0300

    How to detail viewmodal add and navigation setting

---
+++ lib/viewmodel/how_to_detail_viewmodel.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:storyhub/view/how_to_detail_view.dart';
+
+abstract class HowToDetailViewModel extends State<HowToDetailView> {
+  void popFuntion() {
+    Navigator.pop(context);
+  }
+}
