adbc34b6c22e327b90f6624e257890fcbf319ab8
nuray <83471802+nuraybayrakdar@users.noreply.github.com>
2022-08-16 03:06:07 +0300

    AboutUs view added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:storyhub/view/about_us_view.dart';
 import 'package:storyhub/view/game_page_view.dart';
 import 'package:storyhub/view/how_to_detail_view.dart';
 import 'package:storyhub/view/how_to_page_view.dart';
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       //title: 'Flutter Demo',
-      home: MainPageSettings(),
+      home: const AboutUsView(),
     );
   }
 }
