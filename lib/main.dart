fe6bf676d903789da162a5870c5f4785c157b660
alyilmaz99 <aliyilmaz9977@gmail.com>
2022-09-24 00:45:25 +0300

    drawer fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:storyhub/feature/auth/splashscreen/view/splashscreenview.dart';
+import 'package:storyhub/feature/home/final/view/final_page_view.dart';
 import 'package:storyhub/feature/home/gamepage/view/HomeCardsOrder.dart';
 import 'package:storyhub/feature/home/mainpage/view/main_page_view.dart';
 import 'package:storyhub/feature/home/scenario/view/displayscenario.dart';
@@ -49,7 +50,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const SplashScreenView(),
+      home: const HomeCardsOrder(),
     );
   }
 }
