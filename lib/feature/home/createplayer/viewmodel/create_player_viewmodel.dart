41f73d897b923e654e43101f8b8566b08c49a341
alyilmaz99 <aliyilmaz9977@gmail.com>
2022-11-07 22:17:07 +0300

    finish v

--- lib/feature/home/createplayer/viewmodel/create_player_viewmodel.dart
+++ lib/feature/home/createplayer/viewmodel/create_player_viewmodel.dart
@@ -3,10 +3,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:storyhub/feature/home/scenario/view/selectscenarioview.dart';
-import 'package:storyhub/feature/home/voteScreen/view/vote_screen_view.dart';
 import 'package:storyhub/product/model/player_selection_model.dart';
 import '../../../../core/components/playerCarousel/playerCarouselViewModel.dart';
-import '../../../../product/model/button_sound.dart';
 import '../../../settings/model/game_settings_model.dart';
 import '../../voteScreen/model/vote_model.dart';
 import '../model/player_model.dart';
@@ -43,7 +41,6 @@ abstract class CreatePlayerViewModel extends State<CreatePlayerView> {
       decoration: InputDecoration(
         contentPadding: const EdgeInsets.all(10),
         labelText: AppLocalizations.of(context)!.createPlayerName,
-        floatingLabelBehavior: FloatingLabelBehavior.never,
         labelStyle: Theme.of(context).textTheme.bodyMedium?.copyWith(
               color: Colors.white,
               fontWeight: FontWeight.w500,
