bb5762220fd824f5cdac2aacb20c2973a802372f
Ahmet Özyörük <ahmetozyoruk3@hotmail.com>
2022-11-10 01:43:00 +0300

    reduce images

--- assets/images/icons/asd.sh
+++
@@ -1,5 +0,0 @@
-convert icon1.png -quality 60% icon1.jpg
-convert icon2.png -quality 60% icon2.jpg
-convert icon3.png -quality 60% icon3.jpg
-convert icon4.png -quality 60% icon4.jpg
-convert icon5.png -quality 60% icon5.jpg
--- assets/images/splash/asd.sh
+++
@@ -1,4 +0,0 @@
-convert 1.png -quality 60% 1.jpg
-convert 2.png -quality 60% 2.jpg
-convert 3.png -quality 60% 3.jpg
-convert 4.png -quality 60% 4.jpg
